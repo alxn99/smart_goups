0e4ba22c227a1b056f737e852be4d2b6e05ac7c2
florindan23 <dan.florinalexandru@yahoo.com>
2025-12-03 12:14:06 +0200

    Input added

---
+++ input.py
@@ -0,0 +1,5 @@
+def get_name_email():
+    nume=input("Introdu numele")
+    email=input("Introdu adresa de email")
+
+    return nume,email
--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-print("2")
-print("3")
+from input import get_name_email
+
+nume, email = get_name_email()
+print("nume")
+print("email")

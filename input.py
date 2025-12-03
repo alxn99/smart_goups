8699d870b16f10049d539fcb8c428c4f99f6af48
florindan23 <dan.florinalexandru@yahoo.com>
2025-12-03 12:33:10 +0200

    update

--- input.py
+++ input.py
@@ -1,5 +1,5 @@
 def get_name_email():
-    nume=input("Introdu numele")
-    email=input("Introdu adresa de email")
+    nume=input("Introdu numele : ")
+    email=input("Introdu adresa de email : ")
 
     return nume,email
